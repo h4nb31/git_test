c4e56861049db696d5681be2a6834a631e9d3090
amir <edygov.a@posadmin.ru>
2025-09-01 11:48:37 +0300

    struct для записи вместо класса.

--- SseConnector/Program.cs
+++ SseConnector/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace SseConnector;
 
-public record TokenResponse(string AccessToken, string RefreshToken);
+public record struct TokenResponse(string AccessToken, string RefreshToken);
+public record struct EmployeeStatus(string EmployeeUId, int Status);
 
 class Program
 {
@@ -19,6 +21,21 @@ class Program
             Password = "d3f4u7t"
         };
 
+        Dictionary<string, string> EmployeesMap = new()
+        {
+            {"270d6d13-7271-45e6-9f58-de6601af28bf", "Edygov.A"},
+            {"af9f3a32-a19c-4dca-b950-ff1784801656", "Kimryakov.A"},
+            {"b70d8271-f4f9-4bd2-ad91-92dcb196cd8b", "Tremaskin.A"},
+            {"b08ecce0-b9b1-4e6e-a261-6d83de0890af", "Nikulin.P"},
+        };
+
+        Dictionary<int, string> StatusMap = new()
+        {
+            {1 , "Online" },
+            {5 , "Offline" },
+            {0 , "None" },
+        };
+
         var response = await client.PostAsJsonAsync(loginUrl, loginData);
         var tokens = await response.Content.ReadFromJsonAsync<TokenResponse>();
         System.Console.WriteLine(tokens);
@@ -27,15 +44,17 @@ class Program
         while (true) {
             try
             {
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", tokens?.AccessToken);
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", tokens.AccessToken);
                 System.Console.WriteLine("SSE connection");
                 using (var streamReader = new StreamReader(await client.GetStreamAsync(url)))
                 {
                     while (!streamReader.EndOfStream)
                     {
                         var message = await streamReader.ReadLineAsync();
-                        if (message!.StartsWith("data: ")) {
-                            System.Console.WriteLine($"SSE update: {message}");
+                        if (message!.StartsWith("data: "))
+                        {
+                            var jsonMessage = JsonSerializer.Deserialize<EmployeeStatus>(message.Replace("data: ", ""));
+                            System.Console.WriteLine($"SSE update: {EmployeesMap[jsonMessage!.EmployeeUId]} - {StatusMap[jsonMessage!.Status]}");
                         }
                     }    
                 }
