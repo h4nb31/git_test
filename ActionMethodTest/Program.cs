d4e4df998716e5e79a02f88f77897ab6561e4573
amir <edygov.a@posadmin.ru>
2025-09-02 16:12:33 +0300

    переписал Анонимный делигат более наглядно

--- ActionMethodTest/Program.cs
+++ ActionMethodTest/Program.cs
@@ -4,25 +4,31 @@
 
 public class Program
 {
-    public record struct SomeStruct(string Element, int Number);
-    public record struct SecondStruct(double DoubleNum, char Sybmol);
-    
-    public record struct ThirdStruct(string Name, string Place);
+    public record struct First(string StringPar);
+    public record struct Second(double DoublePar);
+    public record struct Third(int IntegerPar);
+
+    static Action<string, double, int> SomeActionMethod = (first, second, third) =>
+    {
+        System.Console.WriteLine("\n\nThis method was invoked from an Action delegate!\n\n");
+        System.Console.WriteLine($"First parameter: {first}");
+        System.Console.WriteLine($"Second parameter: {second}");
+        System.Console.WriteLine($"Third parameter: {third}\n\n");
+    };
 
     public static void Main()
     {
-        var newStruct = new SomeStruct("First", 1);
-        var secondStruct = new SecondStruct(22.2, 'a');
-
-        Action<SomeStruct, SecondStruct, int> SomeActionMethod = (first, second, third) =>
-        {
-            System.Console.WriteLine($"\n\nThis is first struct: \nElement: {first.Element} Number: {first.Number}\n\nThis is second struct: \nDouble: {second.DoubleNum} Symbol: {second.Sybmol}\n\n");
-            System.Console.WriteLine($"just a sum: {first.Number + second.DoubleNum}\n\n");
-            System.Console.WriteLine($"third number: {third}\n\n");
-        };
-
-        SomeActionMethod(newStruct, secondStruct, 5);
-
+        var first = new First("Hello from Action delegate!");
+        var second = new Second(3.14159);
+        var third = new Third(42);
+        SimpleVoidFunc(first.StringPar, second.DoublePar, third.IntegerPar, SomeActionMethod);
 
     }
+
+    public static void SimpleVoidFunc(string str, double dbl, int integer, Action<string, double, int> action)
+    {
+        action?.Invoke(str, dbl, integer);
+    }
+
+
 }
