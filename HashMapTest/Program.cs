7e2cf9d5eebb0c54ed524d0dfc130f92a3c5dcd8
amir <edygov.a@posadmin.ru>
2025-09-04 18:44:58 +0300

    Новый проект для тестирования Хэш таблицы (словарей)

---
+++ HashMapTest/Program.cs
@@ -0,0 +1,11 @@
+﻿var fruitCounts = new Dictionary<string, int>
+{
+    {"apple", 5},
+    {"cherry", 10},
+};
+
+
+if (fruitCounts.TryGetValue("apple", out int count))
+{
+    System.Console.WriteLine($"Кол-во яблок: {count}");
+}
